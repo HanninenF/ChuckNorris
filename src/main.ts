d443ec72f1c253c065585ba410a200f523a617e6
HanninenF <hanninenfredrik@gmail.com>
2024-12-16 17:31:06 +0100

    initial html structure

--- src/main.ts
+++ src/main.ts
@@ -22,45 +22,51 @@ const jokesByCategory = [
 ] as const;
 
 type AllowedCategories = (typeof jokesByCategory)[number];
+const category: AllowedCategories = "explicit";
 
-const category: AllowedCategories = "food";
 const getRandomJoke = async (): Promise<string> => {
   const response = await fetch(url + category);
   const data: { value: string } = await response.json();
-
   return data.value;
 };
 
-console.log(getRandomJoke());
+const displayJokeDropdown = async () => {
+  const sectionElement = document.querySelector("#middle") as HTMLElement;
+  const h2Element = document.createElement("h2") as HTMLHeadElement;
+  h2Element.textContent = "Get Chuck Norris Jokes By Category";
+  const form = document.createElement("form") as HTMLFormElement;
+  form.id = "category-form";
+  form.action = "";
+  const labelElement = document.createElement("label") as HTMLLabelElement;
+  labelElement.htmlFor = "dropdown-menu";
+  labelElement.textContent = "Category";
+  const selectElement = document.createElement("select") as HTMLSelectElement;
+  selectElement.name = "dropdown-menu";
+  selectElement.id = "dropdown-menu";
+  selectElement.setAttribute("required", "true");
+  sectionElement.appendChild(h2Element);
+  sectionElement.appendChild(form);
+  form.appendChild(labelElement);
+  labelElement.appendChild(selectElement);
 
-const displayJoke = async (): Promise<void> => {
-  const divElement = document.querySelector("#joke") as HTMLElement;
-  const pElement = document.createElement("p") as HTMLParagraphElement;
+  const optionPlaceholder = document.createElement(
+    "option"
+  ) as HTMLOptionElement;
+  optionPlaceholder.value = "";
+  optionPlaceholder.setAttribute("disabled", "true");
+  optionPlaceholder.setAttribute("selected", "true");
+  optionPlaceholder.textContent = "-- Select a category --";
+  selectElement.appendChild(optionPlaceholder);
 
-  pElement.textContent = await getRandomJoke();
-  divElement.appendChild(pElement);
+  jokesByCategory.forEach((category) => {
+    const optionElement = document.createElement("option") as HTMLOptionElement;
+    optionElement.value = category;
+    optionElement.textContent = category;
+    selectElement.appendChild(optionElement);
+  });
 };
 
-displayJoke();
-
-[
-  "animal",
-  "career",
-  "celebrity",
-  "dev",
-  "explicit",
-  "fashion",
-  "food",
-  "history",
-  "money",
-  "movie",
-  "music",
-  "political",
-  "religion",
-  "science",
-  "sport",
-  "travel",
-];
+displayJokeDropdown();
 
 /* const url = "https://api.chucknorris.io/jokes/";
 const getRandomJoke = async (): Promise<string> => {
--- src/main2.ts
+++ src/main2.ts
@@ -1,18 +1,36 @@
-const url = "";
+const url = "https://api.chucknorris.io/jokes/random?category=";
+
+const jokesByCategory = [
+  "animal",
+  "career",
+  "celebrity",
+  "dev",
+  "explicit",
+  "fashion",
+  "food",
+  "history",
+  "money",
+  "movie",
+  "music",
+  "political",
+  "religion",
+  "science",
+  "sport",
+  "travel",
+] as const;
+
+type AllowedCategories = (typeof jokesByCategory)[number];
+const category: AllowedCategories = "fashion";
 
 const getRandomJoke = async (): Promise<string> => {
-  const response = await fetch(url + "random");
+  const response = await fetch(url + category);
   const data: { value: string } = await response.json();
-
   return data.value;
 };
 
-console.log(getRandomJoke);
-
-const displayJoke = async (): Promise<void> => {
+const displayJoke = async () => {
   const divElement = document.querySelector("#joke") as HTMLElement;
   const pElement = document.createElement("p") as HTMLParagraphElement;
-
   pElement.textContent = await getRandomJoke();
   divElement.appendChild(pElement);
 };
